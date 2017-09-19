6e00e1e560783dd5c4f7a9291c2192f9e57ca232
asciidisco <public@asciidisco.com>
2017-09-19 17:28:06 +0200

    chore(CI): Setting up CI build

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 import sys
 import os
-print os.path.dirname(__file__)
-print os.path.join(os.path.dirname(__file__))
-sys.path.insert(0, os.path.join(os.path.dirname(__file__)))
-sys.path.insert(0, os.path.join(os.path.dirname(__file__) + '/resources'))
-sys.path.insert(0, os.path.join(os.path.dirname(__file__) + '/resources/lib'))
+print os.path.dirname(os.path.abspath(__file__))
+print os.path.join(os.path.dirname(os.path.abspath(__file__)) + '/resources')
+print os.path.join(os.path.dirname(os.path.abspath(__file__)) + '/resources/lib')
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)) + '/resources'))
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)) + '/resources/lib'))
 print sys.path
 
 import nose

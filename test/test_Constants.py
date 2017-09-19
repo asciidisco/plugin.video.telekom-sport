fa389f35338a0f5f445268e899e9f2699af7ee33
asciidisco <public@asciidisco.com>
2017-09-19 16:28:34 +0200

    chore(CI): Setting up CI build

--- test/test_Constants.py
+++ test/test_Constants.py
@@ -8,8 +8,8 @@
 
 import sys
 import os
-sys.path.insert(0, os.path.join(os.path.dirname(os.path.dirname(__file__)) + '/resources/lib'))
-sys.path.insert(0, os.path.join(os.path.dirname(__file__) + '/resources/lib'))
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.dirname(__file__)), '/resources/lib'))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '/resources/lib'))
 print sys.path
 
 import unittest

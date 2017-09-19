255c8045d2f2e4861fcc3b90a563a217bb8d8e85
asciidisco <public@asciidisco.com>
2017-09-19 16:26:00 +0200

    chore(CI): Setting up CI build

--- test/test_Constants.py
+++ test/test_Constants.py
@@ -8,6 +8,7 @@
 
 import sys
 import os
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.dirname(__file__)) + '/resources/lib'))
 sys.path.insert(0, os.path.join(os.path.dirname(__file__) + '/resources/lib'))
 print sys.path
 

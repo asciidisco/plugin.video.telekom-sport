d4a14301b5629a97052dfe45b509d9b57d67e1bf
asciidisco <public@asciidisco.com>
2017-09-19 17:23:36 +0200

    chore(CI): Setting up CI build

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import sys
 import os
-sys.path.insert(0, os.path.dirname(__file__))
+print os.path.dirname(__file__)
+print os.path.join(os.path.dirname(__file__))
 sys.path.insert(0, os.path.join(os.path.dirname(__file__)))
 sys.path.insert(0, os.path.join(os.path.dirname(__file__) + '/resources'))
 sys.path.insert(0, os.path.join(os.path.dirname(__file__) + '/resources/lib'))

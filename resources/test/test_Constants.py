e87e4f4df847a3c64390fa427cc1336c6eee82ec
asciidisco <public@asciidisco.com>
2017-09-19 13:52:40 +0200

    chore(CI): Setting up CI build

--- resources/test/test_Constants.py
+++ resources/test/test_Constants.py
@@ -9,6 +9,7 @@
 import sys
 from os import path
 sys.path.append(path.dirname(path.dirname(path.dirname(path.abspath(__file__)))))
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
 print sys.path
 

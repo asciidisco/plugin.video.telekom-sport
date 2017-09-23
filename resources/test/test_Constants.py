c641647ea225476c9df4acb347fa909f3e5f1aa9
asciidisco <public@asciidisco.com>
2017-09-23 13:38:15 +0200

    chore(test): Adds test for the Constants module

--- resources/test/test_Constants.py
+++ resources/test/test_Constants.py
@@ -12,20 +12,50 @@ from resources.lib.Constants import Constants
 class ConstantsTestCase(unittest.TestCase):
     """Tests for the `Constants` module"""
 
+    def test_get_addon_id(self):
+        """ADD ME"""
+        constants = Constants()
+        self.assertEqual(
+            first=constants.get_addon_id(),
+            second='plugin.video.telekom-sport')
+
     def test_get_base_url(self):
         """ADD ME"""
         constants = Constants()
-        self.assertEqual(constants.get_base_url(), 'https://www.telekomsport.de')
+        self.assertEqual(
+            first=constants.get_base_url(),
+            second='https://www.telekomsport.de')
 
     def test_get_login_link(self):
         """ADD ME"""
         constants = Constants()
-        self.assertEqual(constants.get_login_link(), 'https://www.telekomsport.de/service/auth/web/login?headto=https://www.telekomsport.de/info')
+        self.assertEqual(
+            first=constants.get_login_link(),
+            second='https://www.telekomsport.de/service/auth/web/login?headto=https://www.telekomsport.de/info')
 
     def test_get_login_endpoint(self):
         """ADD ME"""
         constants = Constants()
-        self.assertEqual(constants.get_login_endpoint(), 'https://accounts.login.idm.telekom.com/sso')
+        self.assertEqual(
+            first=constants.get_login_endpoint(),
+            second='https://accounts.login.idm.telekom.com/sso')
+
+    def test_get_epg_url(self):
+        """ADD ME"""
+        constants = Constants()
+        self.assertEqual(
+            first=constants.get_epg_url(),
+            second='https://www.telekomsport.de/api/v1/')
+
+    def test_get_stream_definition_url(self):
+        """ADD ME"""
+        base = 'https://www.telekomsport.de'
+        route = '/service/player/streamAccess'
+        params = '?videoId=%VIDEO_ID%&label=2780_hls'
+        constants = Constants()
+        self.assertEqual(
+            first=constants.get_stream_definition_url(),
+            second=base + route + params)
 
     def test_get_sports_list(self):
         """ADD ME"""
@@ -41,3 +71,10 @@ class ConstantsTestCase(unittest.TestCase):
         self.assertIn('skybuli', sports_keys)
         self.assertIn('skychamp', sports_keys)
         self.assertIn('skyhandball', sports_keys)
+
+    def test_get_statics_list(self):
+        """ADD ME"""
+        constants = Constants()
+        statics = constants.get_statics_list()
+        statics_keys = statics.keys()
+        self.assertIn('liga3', statics_keys)

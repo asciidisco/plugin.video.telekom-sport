e8d2dad8cbc5a2b4113bdd92e70fd0ab6241eba4
asciidisco <public@asciidisco.com>
2017-09-25 13:54:49 +0200

    chore(docs): Fixes Sphinx docs

--- docs/conf.py
+++ docs/conf.py
@@ -6,19 +6,13 @@
 
 import os
 import sys
+import sphinx_rtd_theme
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)) + os.path.sep)
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + os.path.sep)
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + os.path.sep + 'resources' + os.path.sep)
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + os.path.sep + 'resources' + os.path.sep + 'lib' + os.path.sep)
-import sphinx_rtd_theme
-from recommonmark.parser import CommonMarkParser
-
 
 # -- General configuration ------------------------------------------------
-
-# If your documentation needs a minimal Sphinx version, state it here.
-#
-# needs_sphinx = '1.0'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -31,14 +25,8 @@ extensions = [
     'sphinx.ext.viewcode',
     'm2r']
 
-# Add any paths that contain templates here, relative to this directory.
-# templates_path = ['_templates']
-
 # The suffix(es) of source filenames.
-# You can specify multiple suffix as a list of string:
-#
-# source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
 
 # The master toctree document.
 master_doc = 'index'
@@ -74,82 +62,15 @@ pygments_style = 'sphinx'
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
-source_suffix = ['.rst', '.md']
 
 # -- Options for HTML output ----------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
+# The theme to use for HTML and HTML Help pages.
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-html_static_path = ['../resources/icon.png']
-html_logo = '_static/icon.png'
-
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-#
-# html_theme_options = {}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
-
-
-# -- Options for HTMLHelp output ------------------------------------------
-
-# Output file base name for HTML help builder.
-htmlhelp_basename = 'pluginvideotelekomdoc'
-
-
-# -- Options for LaTeX output ---------------------------------------------
-
-latex_elements = {
-    # The paper size ('letterpaper' or 'a4paper').
-    #
-    # 'papersize': 'letterpaper',
-
-    # The font size ('10pt', '11pt' or '12pt').
-    #
-    # 'pointsize': '10pt',
-
-    # Additional stuff for the LaTeX preamble.
-    #
-    # 'preamble': '',
-
-    # Latex figure (float) alignment
-    #
-    # 'figure_align': 'htbp',
-}
-
-# Grouping the document tree into LaTeX files. List of tuples
-# (source start file, target name, title,
-#  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    (master_doc, 'pluginvideotelekom-sport.tex', u'plugin.video.telekom-sport Documentation',
-     u'asciidisco', 'manual'),
-]
-
-
-# -- Options for manual page output ---------------------------------------
-
-# One entry per manual page. List of tuples
-# (source start file, name, description, authors, manual section).
-man_pages = [
-    (master_doc, 'pluginvideotelekom-sport', u'pluginvideotelekom-sport Documentation',
-     [author], 1)
-]
-
-
-# -- Options for Texinfo output -------------------------------------------
-
-# Grouping the document tree into Texinfo files. List of tuples
-# (source start file, target name, title, author,
-#  dir menu entry, description, category)
-texinfo_documents = [
-    (master_doc, 'pluginvideotelekom-sport', u'pluginvideotelekom-sport Documentation',
-     author, 'pluginvideotelekom-sport', 'One line description of project.',
-     'Miscellaneous'),
-]
+html_static_path = ['../resources/icon.png']
+html_logo = '_static/icon.png'

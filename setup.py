a1f471bc47d6bd3fe4d5f911d5713159ffe1ff29
asciidisco <public@asciidisco.com>
2017-09-25 16:29:01 +0200

    chore(docs): Improve Sphinx docs metadata

--- setup.py
+++ setup.py
@@ -11,8 +11,6 @@ import re
 import sys
 from setuptools import find_packages, setup
 
-AUTHOR_EMAIL = 'public@asciidisco.com'
-URL = 'https://github.com/asciidisco/plugin.video.telekom-sport'
 REQUIRED_PYTHON_VERSION = (2, 7)
 PACKAGES = find_packages()
 INSTALL_DEPENDENCIES = []
@@ -51,42 +49,55 @@ def get_addon_data():
     root_dir = os.path.dirname(os.path.abspath(__file__))
     pathname = os.path.join(root_dir, 'addon.xml')
     with open(pathname, 'rb') as addon_xml:
+        addon_xml_contents = addon_xml.read()
+        _id = re.search(
+            r'(?<!xml )id="(.+?)"',
+            addon_xml_contents).group(1)
         author = re.search(
             r'(?<!xml )provider-name="(.+?)"',
-            addon_xml.read()).group(1)
+            addon_xml_contents).group(1)
         name = re.search(
             r'(?<!xml )name="(.+?)"',
-            addon_xml.read()).group(1)
+            addon_xml_contents).group(1)
         version = re.search(
             r'(?<!xml )version="(.+?)"',
-            addon_xml.read()).group(1)
+            addon_xml_contents).group(1)
         desc = re.search(
             r'(?<!xml )description lang="en_GB">(.+?)<',
-            addon_xml.read()).group(1)
+            addon_xml_contents).group(1)
+        email = re.search(
+            r'(?<!xml )email>(.+?)<',
+            addon_xml_contents).group(1)
+        source = re.search(
+            r'(?<!xml )email>(.+?)<',
+            addon_xml_contents).group(1)
         return {
+            'id': _id,
             'author': author,
             'name': name,
             'version': version,
             'desc': desc,
+            'email': email,
+            'source': source,
         }
 
 
 if sys.version_info < REQUIRED_PYTHON_VERSION:
     sys.exit('Python >= 2.7 is required. Your version:\n' + sys.version)
 
-ADDON_DATA = get_addon_data()
-
-setup(
-    name=ADDON_DATA.get('name'),
-    version=ADDON_DATA.get('version'),
-    author=ADDON_DATA.get('author'),
-    author_email=AUTHOR_EMAIL,
-    description=ADDON_DATA.get('desc'),
-    packages=PACKAGES,
-    include_package_data=True,
-    install_requires=INSTALL_DEPENDENCIES,
-    setup_requires=SETUP_DEPENDENCIES,
-    tests_require=TEST_DEPENDENCIES,
-    extras_require=EXTRA_DEPENDENCIES,
-    test_suite='nose.collector',
-)
+if __name__ == '__main__':
+    ADDON_DATA = get_addon_data()
+    setup(
+        name=ADDON_DATA.get('name'),
+        version=ADDON_DATA.get('version'),
+        author=ADDON_DATA.get('author'),
+        author_email=ADDON_DATA.get('email'),
+        description=ADDON_DATA.get('desc'),
+        packages=PACKAGES,
+        include_package_data=True,
+        install_requires=INSTALL_DEPENDENCIES,
+        setup_requires=SETUP_DEPENDENCIES,
+        tests_require=TEST_DEPENDENCIES,
+        extras_require=EXTRA_DEPENDENCIES,
+        test_suite='nose.collector',
+    )

14749518fda0d8b00d676a3bd11dd5561401df69
asciidisco <public@asciidisco.com>
2017-10-04 15:46:11 +0200

    fix(settings): Plugin should work without pyDes installed
    1

--- resources/lib/Settings.py
+++ resources/lib/Settings.py
@@ -4,27 +4,48 @@
 # Created on: 24.07.2017
 # License: MIT https://goo.gl/WA1kby
 
-"""ADD ME"""
+"""Settings interface for Kodi, includes en-/decryption of credentials"""
 
 import base64
 import uuid
 import time
 import xbmc
-import pyDes
+try:
+    import pyDes
+except ImportError:
+    USE_PYDES = False
+else:
+    USE_PYDES = True
 
 
 class Settings(object):
-    """ADD ME"""
+    """Settings interface for Kodi, includes en-/decryption of credentials"""
 
     def __init__(self, utils, dialogs, constants):
-        """ADD ME"""
+        """
+        Injects instances, sets encryption var & addon_id
+
+        :param utils: Utils instance
+        :type utils: resources.lib.Utils
+        :param dialogs: Dialogs instance
+        :type dialogs: resources.lib.Dialogs
+        :param constants: Constants instance
+        :type constants: resources.lib.Constants
+        """
         self.utils = utils
         self.dialogs = dialogs
         self.constants = constants
+        self.use_encryption = USE_PYDES
         self.addon_id = self.constants.get_addon_id()
 
     def uniq_id(self, delay=1):
-        """ADD ME"""
+        """
+        Returns a unique id based on the devices MAC address
+
+        :param delay: Retry delay in sec
+        :type delay: int
+        :returns:  string -- Unique secret
+        """
         mac_addr = self.__get_mac_address(delay=delay)
         if ':' in mac_addr and delay == 2:
             return uuid.uuid5(uuid.NAMESPACE_DNS, str(mac_addr)).bytes
@@ -35,7 +56,13 @@ class Settings(object):
             return 'UnsafeStaticSecret'
 
     def encode(self, data):
-        """ADD ME"""
+        """
+        Encodes data
+
+        :param data: Data to be encoded
+        :type data: str
+        :returns:  string -- Encoded data
+        """
         key_handle = pyDes.triple_des(
             self.uniq_id(delay=2),
             pyDes.CBC,
@@ -46,7 +73,13 @@ class Settings(object):
         return base64.b64encode(s=encrypted)
 
     def decode(self, data):
-        """ADD ME"""
+        """
+        Decodes data
+
+        :param data: Data to be decoded
+        :type data: str
+        :returns:  string -- Decoded data
+        """
         key_handle = pyDes.triple_des(
             self.uniq_id(delay=2),
             pyDes.CBC,
@@ -57,19 +90,27 @@ class Settings(object):
         return decrypted
 
     def has_credentials(self):
-        """ADD ME"""
+        """
+        checks if credentials are set
+
+        :returns:  bool -- Credentials set
+        """
         addon = self.utils.get_addon()
         user = addon.getSetting('email')
         password = addon.getSetting('password')
         return user != '' or password != ''
 
     def set_credentials(self):
-        """ADD ME"""
+        """
+        Opens up the email & password dialogs and stores entered credentials
+
+        :returns:  tuple -- Credential pai
+        """
         addon = self.utils.get_addon()
         user = self.dialogs.show_email_dialog()
         password = self.dialogs.show_password_dialog()
         do_encrypt = addon.getSetting('encrypt_credentials')
-        if do_encrypt == 'True':
+        if do_encrypt == 'True' and self.use_encryption is True:
             _mail = self.encode(user)
             _password = self.encode(password)
         else:
@@ -80,7 +121,11 @@ class Settings(object):
         return (user, password)
 
     def get_credentials(self):
-        """ADD ME"""
+        """
+        Returns credentials in clear text
+
+        :returns:  tuple -- Clear text credentials
+        """
         addon = self.utils.get_addon()
         user = addon.getSetting('email')
         password = addon.getSetting('password')
@@ -89,8 +134,14 @@ class Settings(object):
         return (self.decode(user), self.decode(password))
 
     @classmethod
-    def __get_mac_address(cls, delay):
-        """ADD ME"""
+    def __get_mac_address(cls, delay=1):
+        """
+        Returns the users mac address
+
+        :param delay: retry delay in sec
+        :type delay: int
+        :returns:  string -- Devices MAC address
+        """
         mac_addr = xbmc.getInfoLabel('Network.MacAddress')
         # hack response busy
         i = 0
